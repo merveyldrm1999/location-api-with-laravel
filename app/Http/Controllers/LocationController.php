aef1c7c0a671b761bb87db1ccd15168251de7991
MerveYildirim <merveyldrm890@gmail.com>
2024-10-28 14:50:15 +0300

    Service ve Controller oluşturuldu.

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\LocationService;
 use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
+    protected LocationService $locationService;
+
+    public function __construct(LocationService $locationService)
+    {
+        $this->locationService = $locationService;
+    }
+
 
     public function index()
     {
-        //
+
     }
 
 
