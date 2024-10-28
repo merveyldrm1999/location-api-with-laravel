0ced7a7aa0d171cdeee005fd368ca6a3332ca380
MerveYildirim <merveyldrm890@gmail.com>
2024-10-28 16:06:09 +0300

    Put işlemi sırasında veririnin var olup olmadığı kontrolu request içerisinde tanımlandı.

--- app/Http/Requests/LocationRequest.php
+++ app/Http/Requests/LocationRequest.php
@@ -16,19 +16,39 @@ class LocationRequest extends FormRequest
     public function rules(): array
     {
 //        'regex:/^#?([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/', ifadesi hex rengi kontrol eder.
-
-        return [
+        $rules = [
             'name' => ['required', 'string'],
             'latitude' => ['required', 'numeric'],
             'longitude' => ['required', 'numeric'],
             'hex' => ['required', 'string', 'regex:/^#?([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/'],
         ];
+
+        //Eğer put veya delete metodu ise id kontrolü yapılacak.
+        if ($this->isMethod('put')) {
+            $rules = array_merge($rules, [
+                'location' => ['required', 'integer', 'exists:locations,id']
+            ]);
+        }
+
+        return $rules;
+    }
+
+
+    //Body içerisine location id'sini ekledik ve bu id'yi kontrol etmeyi sağladık.
+    public function all($keys = null): array
+    {
+        $data = parent::all();
+        if ($this->isMethod('put') || $this->isMethod('delete')) {
+            $data['location'] = $this->route('location');
+        }
+        return $data;
     }
 
 
     public function attributes(): array
     {
         return [
+            'id' => 'Id',
             'name' => 'Name',
             'latitude' => 'Latitude',
             'longitude' => 'Longitude',
