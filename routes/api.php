f7f29bac15d978e411796d3af640a8ad79e251c2
MerveYildirim <merveyldrm890@gmail.com>
2024-10-28 15:42:46 +0300

    Rota show için düzenlendi.

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 //1 dakika içinde 60 istekten fazla yapılmaması için throttle middleware kullanıldı.
 Route::middleware('throttle:60,1')->group(function () {
-    Route::prefix("locations")->group(function () {
-        Route::resource('', \App\Http\Controllers\LocationController::class)->only(['store']);
-    });
+    Route::resource('locations', \App\Http\Controllers\LocationController::class)
+        ->only(['store', 'show'])
+        ->where(['location' => '[0-9]+']);
 });
