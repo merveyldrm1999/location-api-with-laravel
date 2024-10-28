89e7649443691a81819118de2b0d98dfe10bd44e
MerveYildirim <merveyldrm890@gmail.com>
2024-10-28 16:05:14 +0300

    Rotaya update eklendi

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 //1 dakika içinde 60 istekten fazla yapılmaması için throttle middleware kullanıldı.
 Route::middleware('throttle:60,1')->group(function () {
     Route::resource('locations', \App\Http\Controllers\LocationController::class)
-        ->only(['store', 'show'])
+        ->only(['store', 'show', 'update'])
         ->where(['location' => '[0-9]+']);
 });
