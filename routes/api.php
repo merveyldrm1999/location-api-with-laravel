91d24e0f7245943bc35cfd2cd5d28337a9ed4394
MerveYildirim <merveyldrm890@gmail.com>
2024-10-29 13:40:27 +0300

    Döküman hazırlama sırasında anlam uyuşmazlıkları fark edilip çözüldü.

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ Route::middleware('throttle:60,1')->group(function () {
 
     Route::prefix('locations')->group(function () {
         Route::get('with-destroy', [\App\Http\Controllers\LocationController::class, 'withDestroy']);
-        Route::post('route', [\App\Http\Controllers\DistanceController::class, 'route']);
-        Route::post('route/{id}', [\App\Http\Controllers\DistanceController::class, 'show'])->where(['location' => '[0-9]+']);
+        Route::post('distance', [\App\Http\Controllers\DistanceController::class, 'route']);
+        Route::post('distance/{id}', [\App\Http\Controllers\DistanceController::class, 'show'])->where(['location' => '[0-9]+']);
     });
 
 });
